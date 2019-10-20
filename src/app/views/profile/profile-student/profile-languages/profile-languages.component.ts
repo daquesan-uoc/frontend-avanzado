9c97e229775e483748231c4d386c81689289e6ad
frontend-avanzado <daquesan@uoc.edu>
2019-10-20 13:36:44 +0200

    Borrar idioma

--- src/app/views/profile/profile-student/profile-languages/profile-languages.component.ts
+++ src/app/views/profile/profile-student/profile-languages/profile-languages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { ProfileService } from '../../../../shared/services/profile.service';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-profile-languages',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-languages.component.scss']
 })
 export class ProfileLanguagesComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router,
+    private profileService: ProfileService) { }
 
   ngOnInit() {
+    // Se recupera el usuario conectado
+    this.user = this.profileService.user;
+
+    // Se comprueba que haya un usuario conectado o se vuelve a la
+    // página de inicio
+    if (!Object.keys(this.user).length) {
+      this.router.navigate(['']);
+    }
   }
 
+  delete(uid: number) {
+    this.user.languages = this.user.languages.filter(item => item.uid !== uid);
+    this.profileService.updateProfile(this.user);
+    this.reload();
+  }
+
+  back(){
+    this.location.back();
+  }
+
+  reload(){
+    this.router.navigate['/profile/profile-student/profile-languages'];
+  }
 }
